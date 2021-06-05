ab61c073deb9979d83f0f5c33b9adb319e558484
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2021-06-05 21:15:57 +0200

    Fixed a bug that affected how pieces were displayed

--- TextGameUI.java
+++ TextGameUI.java
@@ -13,14 +13,14 @@ public class TextGameUI extends GameUI {
     public static final String WHITE_SQUARE_UNICODE = "\u25A0";
     public static final String BLACK_SQUARE_UNICODE = "\u25A1";
 
-    /*
+    
     public static final String HORIZONTAL_LINE_UNICODE = "\u2015";  // ―
     public static final String VERTICAL_LINE_UNICODE = "\u2758";    // ❘
     public static final String TOP_LEFT_CORNER_UNICODE = "\u231C";  // ⌜
     public static final String TOP_RIGHT_CORNER_UNICODE = "\u231D"; // ⌝
     public static final String BOTTOM_LEFT_CORNER_UNICODE = "\u231E"; // ⌞
     public static final String BOTTOM_RIGHT_CORNER_UNICODE = "\u231F"; // ⌟
-    */
+    
 
     public TextGameUI() {
         super();
@@ -40,27 +40,40 @@ public class TextGameUI extends GameUI {
         return " " + (c.getColor() ? TextGameUI.WHITE_SQUARE_UNICODE : TextGameUI.BLACK_SQUARE_UNICODE) + " ";
     }
 
+    private String getHL() { return TextGameUI.HORIZONTAL_LINE_UNICODE; }
+
+    private String getVL() { return TextGameUI.VERTICAL_LINE_UNICODE; }
+
+    private String getTLC() { return TextGameUI.TOP_LEFT_CORNER_UNICODE; }
+
+    private String getTRC() { return TextGameUI.TOP_RIGHT_CORNER_UNICODE; }
+
+    private String getBLC() { return TextGameUI.BOTTOM_LEFT_CORNER_UNICODE; }
+
+    private String getBRC() { return TextGameUI.BOTTOM_RIGHT_CORNER_UNICODE; }
+
     private String horizontalLine() {
         String l = "";
-        for(int i = 0; i < Board.NB_COLUMNS; i++) { l += "―――"; }
+        String hr = getHL() + getHL() + getHL(); // "―――"
+        for(int i = 0; i < Board.NB_COLUMNS; i++) { l += hr; }
         return l;
     }
 
     public void drawBoard(Board b) {
         int col = 1;
         int line = Board.NB_LINES; // start at a1
-        System.out.println("  ⌜"+horizontalLine()+"⌝");
+        System.out.println("  "+getTLC()+horizontalLine()+getTRC());
         while(line > 0) {
-            System.out.print(line + " ❘");
+            System.out.print(line + " " + getVL());
             for(col=1; col <= Board.NB_COLUMNS; col++) {
                 System.out.print(drawCell(b.getCell(col, line)));
             }
             line--;
             if(line > 0) {
-                System.out.println("❘");
+                System.out.println(getVL()); // "❘"
             } else {
-                System.out.println("❘");
-                System.out.println("  ⌞"+horizontalLine()+"⌟");
+                System.out.println(getVL()); // "❘"
+                System.out.println("  "+getBLC()+horizontalLine()+getBRC());
                 System.out.println("    a  b  c  d  e  f  g  h ");
             }
         }
@@ -68,7 +81,7 @@ public class TextGameUI extends GameUI {
     }
 
     public void drawUI(Board b) {
-        // clearScreen();
+        clearScreen();
         drawBoard(b);
     }
 
